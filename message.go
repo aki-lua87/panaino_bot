c89e72bee7ec0049ef011c3f3ebadefa217d9830
aki_lua87 <aki_lua87@outlook.jp>
2020-05-09 18:55:08 +0900

    いやな部分改修、MTG機能追加

--- message.go
+++ message.go
@@ -14,93 +14,56 @@ import (
 
 // 一問一答形式メッセージ
 func messageCheck(message string) string {
-
-	switch {
-	// プログラム的処理が必要なもの
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "mtg")):
-		cardName := strings.TrimLeft(message, appConfig.BotName)
-		cardName = strings.TrimLeft(cardName, " mtg ")
-		return FetchHareruyaCards(cardName)
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "セリフ")):
-		return appConfig.SpreadsheetURL
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "おひる")):
-		return GetHirumeshi()
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "天気")):
-		return GetWether("130010")
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "東京の天気")):
-		return GetWether("130010")
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "福岡の天気")):
-		return GetWether("400040")
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "大阪の天気")):
-		return GetWether("270000")
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "明日の緊急")):
-		return GetPESO2EmergencyQuestString("明日", time.Now().UTC().Add(time.Hour*9).Add(time.Hour*24))
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "今日の緊急")):
-		return GetPESO2EmergencyQuestString("今日", time.Now().UTC().Add(time.Hour*9))
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "緊急")):
-		return GetPESO2EmergencyQuestString("今日", time.Now().UTC().Add(time.Hour*9))
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName, "覇者")):
-		text, _ := GetPSO2CoatOfArms()
-		return text
-	}
-
-	// クソクソクソクソクソクソクソクソクソクソクソクソ
-	switch {
-	// プログラム的処理が必要なもの
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "mtg")):
-		cardName := strings.TrimLeft(message, appConfig.BotName2)
-		cardName = strings.TrimLeft(cardName, " mtg ")
-		return FetchHareruyaCards(cardName)
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "セリフ")):
-		return appConfig.SpreadsheetURL
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "おひる")):
-		return GetHirumeshi()
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "天気")):
-		return GetWether("130010")
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "東京の天気")):
-		return GetWether("130010")
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "福岡の天気")):
-		return GetWether("400040")
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "大阪の天気")):
-		return GetWether("270000")
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "明日の緊急")):
-		return GetPESO2EmergencyQuestString("明日", time.Now().UTC().Add(time.Hour*9).Add(time.Hour*24))
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "今日の緊急")):
-		return GetPESO2EmergencyQuestString("今日", time.Now().UTC().Add(time.Hour*9))
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "緊急")):
-		return GetPESO2EmergencyQuestString("今日", time.Now().UTC().Add(time.Hour*9))
-	case strings.HasPrefix(message, fmt.Sprintf("%s %s", appConfig.BotName2, "覇者")):
-		text, _ := GetPSO2CoatOfArms()
-		return text
-	}
-
-	// リファクタリング中
 	if strings.HasPrefix(message, appConfig.BotName) {
-		switch {
-		case strings.Contains(message, "お昼"), strings.Contains(message, "昼飯"), strings.Contains(message, "晩飯"), strings.Contains(message, "ばんめし"), strings.Contains(message, "ひるめし"), strings.Contains(message, "おひる"), strings.Contains(message, "夕飯"):
-			return GetHirumeshi()
-		case strings.Contains(message, "酒"):
-			return GetSake()
-		case strings.Contains(message, "の実況"):
-			return getTodayJikkyou()
-		}
-		return getGSSMessage(strings.TrimLeft(message, appConfig.BotName+" "))
+		return createMessage(message, appConfig.BotName)
 	}
 
 	if strings.HasPrefix(message, appConfig.BotName2) {
-		switch {
-		case strings.Contains(message, "お昼"), strings.Contains(message, "昼飯"), strings.Contains(message, "晩飯"), strings.Contains(message, "ばんめし"), strings.Contains(message, "ひるめし"), strings.Contains(message, "おひる"), strings.Contains(message, "夕飯"):
-			return GetHirumeshi()
-		case strings.Contains(message, "酒"):
-			return GetSake()
-		case strings.Contains(message, "の実況"):
-			return getTodayJikkyou()
-		}
-		return getGSSMessage(strings.TrimLeft(message, appConfig.BotName2+" "))
+		return createMessage(message, appConfig.BotName2)
 	}
-	return getGSSMessage("")
-	// マッチしない場合はGoogleスプレッドシート or 基本セットより取得(一問一答形式)
+	return "んにゃぴ"
+}
 
+func createMessage(message string, botName string) string {
+	switch {
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "晴れる屋")):
+		cardName := strings.TrimLeft(message, botName)
+		cardName = strings.TrimLeft(cardName, " 晴れる屋 ")
+		return FetchHareruyaCards(cardName)
+	case strings.Contains(message, "お昼"), strings.Contains(message, "昼飯"), strings.Contains(message, "晩飯"), strings.Contains(message, "ばんめし"), strings.Contains(message, "ひるめし"), strings.Contains(message, "おひる"), strings.Contains(message, "夕飯"):
+		return GetHirumeshi()
+	case strings.Contains(message, "酒"):
+		return GetSake()
+	case strings.Contains(message, "の実況"):
+		return getTodayJikkyou()
+	case strings.Contains(message, "mtg"):
+		return RandMTGjp()
+	case strings.Contains(message, "メカゴジラ"):
+		return getMekaGozzira()
+		// ここから未リファクタリング
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "セリフ")):
+		return appConfig.SpreadsheetURL
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "おひる")):
+		return GetHirumeshi()
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "天気")):
+		return GetWether("130010")
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "東京の天気")):
+		return GetWether("130010")
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "福岡の天気")):
+		return GetWether("400040")
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "大阪の天気")):
+		return GetWether("270000")
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "明日の緊急")):
+		return GetPESO2EmergencyQuestString("明日", time.Now().UTC().Add(time.Hour*9).Add(time.Hour*24))
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "今日の緊急")):
+		return GetPESO2EmergencyQuestString("今日", time.Now().UTC().Add(time.Hour*9))
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "緊急")):
+		return GetPESO2EmergencyQuestString("今日", time.Now().UTC().Add(time.Hour*9))
+	case strings.HasPrefix(message, fmt.Sprintf("%s %s", botName, "覇者")):
+		text, _ := GetPSO2CoatOfArms()
+		return text
+	}
+	return getGSSMessage(strings.TrimLeft(message, botName+" "))
 }
 
 func getGSSMessage(key string) string {
@@ -232,4 +195,13 @@ func getTodayJikkyou() string {
 	randNum1 := rand.Intn(len(GameList))
 	randNum2 := rand.Intn(len(HitoList))
 	return HitoList[randNum2] + "の" + GameList[randNum1] + "実況"
+}
+
+func getMekaGozzira() string {
+	var List []string
+	List = append(List,
+		"飛行", "先制攻撃", "接死", "呪禁", "絆魂", "威迫", "到達", "トランプル", "警戒", "＋１/＋１",
+	)
+	randNum := rand.Intn(len(List))
+	return List[randNum]
 }
--- mtg_scraping.go
+++ mtg_scraping.go
@@ -1,8 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func randMTGCard() string {
-	return "hogr"	
+	return "hogr"
 }
 
 func FetchHareruyaCards(cardName string) string {
@@ -18,4 +23,26 @@ func FetchHareruyaCards(cardName string) string {
 	// HTML内から最上位のカードのリンク先URLを取得
 	// 取得したURLを返却
 	return reqURL
+}
+
+func RandMTGjp() string {
+	var text string
+	req1, err := http.NewRequest("GET", "http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random", nil)
+	if err != nil {
+		fmt.Println(err)
+		return "エラー"
+	}
+	req1.Header.Set("Accept-Language", "ja,en-US;q=0.7,en;q=0.3")
+	cli := new(http.Client)
+	resp, err := cli.Do(req1)
+	if err == nil {
+		defer resp.Body.Close()
+	}
+	urlLeft := "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid="
+	SliceURL := strings.Split(resp.Request.URL.String(), "=")
+	urlRight := "&type=card"
+
+	text = urlLeft + SliceURL[1] + urlRight
+
+	return text
 }
